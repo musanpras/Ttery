8b252585abd8a0cf8d21b634af558dec54bcbd66
musanpras <muhammadsandy171@gmail.com>
2026-05-22 10:41:34 +0800

    Initial Commit

---
+++ Team17Project/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Team17Project
+//
+//  Created by Muhammad Sandy Prastyo on 22/05/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Team17ProjectTests/Team17ProjectTests.swift
@@ -0,0 +1,19 @@
+//
+//  Team17ProjectTests.swift
+//  Team17ProjectTests
+//
+//  Created by Muhammad Sandy Prastyo on 22/05/26.
+//
+
+import Testing
+@testable import Team17Project
+
+struct Team17ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
